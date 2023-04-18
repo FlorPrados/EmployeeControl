c57a427fec681e16623e88caceddd796ef2429c4
Florencia <florprados@gmail.com>
2023-04-18 11:00:25 -0300

    first commit

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeControl.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeControl
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+}
---
+++ Entidades/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeControl.Entidades
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
